64de625926b7aedd6188ec6c498a376f3813841c
Mads Jakob Steffansen <mads.steffansen@cb.dk>
2017-03-10 20:58:08 +0100

    A little more sensing

--- MJS.GPIO/IRSensor.py
+++ MJS.GPIO/IRSensor.py
@@ -1,6 +1,17 @@
-
+import RPi.GPIO as GPIO
 
 class IRSensor:
 	_pin1 = 0
 	def __init__(self, pin1):
 		self._pin1 = pin1
+		GPIO.setup(_pin1, GPIO.IN)
+		GPIO.add_event_detect(_pin, GPIO.RISING, callback=self.SensorBlocked)
+	def SensorBlocked(self, channel):
+		print("Sensor blocked")
+
+
+
+if __name__ = "__main__":
+	print("Testing IRSensor")
+else:
+	print("Including " + __name__)
