0b6f43b966f721fe328cc1d17d89691191584d77
Mads Jakob Steffansen <mads.steffansen@gmail.com>
2017-03-12 19:35:24 +0000

    Stucture of the repo!

--- test.py
+++ test.py
@@ -1,4 +1,26 @@
-print "Hest"
+import sys
 
-for x in range(0, 10):
-	print "Ko%d" % (x)
+sys.path.append("./MJS")
+
+import GPIO as gp 
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+ir = [5, 6, 13, 19]
+
+for sensor in ir:
+	GPIO.setup(sensor, GPIO.IN)
+
+while 1:
+ 	for sensor in ir:
+		if GPIO.input(sensor):
+			print str(sensor) + " is high"
+		else:
+			print str(sensor) + " is low"
+	time.sleep(1)
+
+
+GPIO.cleanup()
